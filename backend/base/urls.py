c0e4c02009e9e757106e2cf6e0e3e9094095f3a8
Bramstorp <m.bramstorp@stofanet.dk>
2021-02-20 19:51:52 +0100

    added api backend folder

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api.views import api
+
 urlpatterns = [
+    path("api/", api, name="upload"),
     path("admin/", admin.site.urls),
 ]
