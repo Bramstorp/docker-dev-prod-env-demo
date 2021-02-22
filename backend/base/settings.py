0c404eb884361c8f0e1d93b4f8bfa6ff821c16bf
Bramstorp <m.bramstorp@stofanet.dk>
2021-02-22 21:45:53 +0100

    debug update

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=1))
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
